e0cb1fb152be7869a039a8147e84948d8748ac08
aseycan <aseycan@gmail.com>
2024-07-11 14:52:11 +0500

    son_

--- app.py
+++ app.py
@@ -113,3 +113,6 @@ if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(host='0.0.0.0', port=5000, debug=True)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
