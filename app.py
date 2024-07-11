03c150def182e02cd81ebbba32a9fe0a9b256cbf
aseycan <aseycan@gmail.com>
2024-07-11 16:32:12 +0500

    webhook

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ schedule.every().day.do(clean_downloads)
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
